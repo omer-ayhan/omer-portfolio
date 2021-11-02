73d26398a26a030afc15c233237adf6237b7c798
Ömer Ayhan <om.ayhan247@gmail.com>
2021-11-02 17:29:34 +0300

    converted to typecript

--- context/fallbackStorage.ts
+++ context/fallbackStorage.ts
@@ -1,14 +1,23 @@
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
+type StorageTypes =
+  | string
+  | number
+  | boolean
+  | number[]
+  | string[]
+  | symbol
+  | null;
+
 const createNoopStorage = () => {
   return {
-    getItem(_key) {
+    getItem(_key: string | null) {
       return Promise.resolve(null);
     },
-    setItem(_key, value) {
+    setItem(_key: string | null, value: StorageTypes) {
       return Promise.resolve(value);
     },
-    removeItem(_key) {
+    removeItem(_key: string | null) {
       return Promise.resolve();
     },
   };
